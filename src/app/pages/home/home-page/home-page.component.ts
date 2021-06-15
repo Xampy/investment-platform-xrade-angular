d09b36e858e1edac0f0c081eb3a2cf34276f4694
Xampy <xampydev@gmail.com>
2021-06-15 05:57:11 +0100

    Update the dasboard with provided text, update icon on navbar

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AccountDataManagerService } from 'src/app/services/account/account-data-manager.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { AccountDataManagerService } from 'src/app/services/account/account-data
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
     isAuth: boolean = false;
 
     constructor(
@@ -24,6 +24,42 @@ export class HomePageComponent implements OnInit {
             }
             }
         )
+    }
+    ngAfterViewInit(): void {
+        
+
+
+        var mybutton = document.getElementById("myBtn");
+        var homeMenu = document.getElementById("home-menu");
+        var menuLinks = document.querySelectorAll(".link-data-item a");
+        console.log(homeMenu);
+        console.log(menuLinks)
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            homeMenu.classList.add("home-menu-active");
+            menuLinks.forEach(element => {
+                element.classList.add('link-data-item-active')
+            });
+        } else {
+            homeMenu.classList.remove("home-menu-active");
+            menuLinks.forEach(element => {
+                element.classList.remove('link-data-item-active')
+            });
+        }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        }
+
+
+
     }
 
     ngOnInit(): void {
